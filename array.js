a364b2dc2bc67e8ec1411823818d8833662c66c4
Thiago Henrique <thiagohenriquedev@gmail.com>
2021-10-11 21:54:35 -0300

    modificado

--- array.js
+++ array.js
@@ -1,7 +1,19 @@
-var lista = ["macã","laranja","banana"];
+var fruta = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxo"}]
+console.log(fruta);
+alert(fruta[1].nome)
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.cor)
+alert(fruta.cor)*/
+
+
+
+
+// var lista = ["macã","laranja","banana"];
 //lista.push("uva") Adicionar elemento
 //lista.pop(); Tirar o ultimo elemento da lista
-console.log(lista);
+// console.log(lista);
 // console.log(lista.join("-")) Acrescenta - e tira as virgula
 // console.log(lista.toString()); Perde a referencia de array, ou seja, converte em String
 // console.log(lista.reverse()); Elementos ao contrario na lista
