b07219677cd21365ba8a07ee60fc5f6c2d43ef5c
Thiago Henrique <thiagohenriquedev@gmail.com>
2021-10-11 21:46:50 -0300

    modificado

---
+++ array.js
@@ -0,0 +1,10 @@
+var lista = ["macã","laranja","banana"];
+//lista.push("uva") Adicionar elemento
+//lista.pop(); Tirar o ultimo elemento da lista
+console.log(lista);
+// console.log(lista.join("-")) Acrescenta - e tira as virgula
+// console.log(lista.toString()); Perde a referencia de array, ou seja, converte em String
+// console.log(lista.reverse()); Elementos ao contrario na lista
+        
+
+//alert(lista.length); numeros de elementos na lista
