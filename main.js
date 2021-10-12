3b88cdb7bbbe13f380e3cffceb20529ccc215076
Thiago Henrique <thiagohenriquedev@gmail.com>
2021-10-11 21:24:08 -0300

    modificado

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 var nome = "Thiago Henrique";
 var idade = 29;
-alert(nome + " tem " + idade + " anos");
+var idade2 = 10
+var frase = "Corinthians é o melhor time do mundo"
+        //alert(nome + " tem " + idade + " anos");
+        //alert(idade + idade2);
+    console.log(nome)
+    console.log(idade)
+    console.log(frase.replace("Corinthians","Brasil"));
+        //console.log(frase.toLowerCase()); string minuscula
+        //console.log(frase.toUpperCase()); STRING MAIUSCULA
+        //alert(frase.replace("Corinthians","Brasil"));
