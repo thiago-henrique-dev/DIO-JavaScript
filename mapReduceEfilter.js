d56da125a5c9977cf03e9defe6cd8ad671508eca
Thiago Henrique <thiagohenriquedev@gmail.com>
2021-10-17 21:22:08 -0200

    edit

--- mapReduceEfilter.js
+++ mapReduceEfilter.js
@@ -7,7 +7,7 @@ function dobro(num) {
 const resultado = numeros.map(dobro)
 console.log(resultado)*/
 
-const array = [
+/*onst produtos = [
     { nome: "", preco: 23.67, desconto: 0.2},
     { nome: "", preco: 120.89, desconto: 0.3},
     { nome: "", preco: 3567.67, desconto: 0.5},
@@ -16,6 +16,7 @@ const array = [
     { nome: "", preco: 12355, desconto: 0.15},
     
 ]
+
 
 function getCustos(precoComDesconto) {
     return parseInt(precoComDesconto * 0.3)
@@ -27,6 +28,23 @@ function getPrecoComDesconto (produto) {
     return produto.preco * (1 - produto.desconto)
     
 }
+function altoCusto(produto) {
+    return produto.preco >= 1000
+    
+}
 
-const resultado = array.map(getPrecoComDesconto).map(getCustos)
+const resultado = produtos
+    .filter(altoCusto)
+    .map(getPrecoComDesconto)
+    .map(getCustos)
 console.log(resultado)
+
+/*const notas = [7.1, 8.3, 6.3, 7.7, 9.1, 4.3]
+
+function aprovado(nota) {
+    return nota >= 7
+}
+
+const resultado = notas.filter(aprovado)
+console.log(resultado)*/
+
